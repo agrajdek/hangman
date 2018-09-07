5c4f8a43df416bc6c9641dd7595e6f97b6e48c87
agrajdek <aagata.grajdek@gmail.com>
2018-09-07 12:33:11 +0200

    file_scanner_in_test
    Added file scanner in test

--- sengitaa/hangman/Main.java
+++ sengitaa/hangman/Main.java
@@ -1,55 +1,64 @@
 package futureprogrammer.sengitaa.hangman;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         String testWord = "hangman";
         int index = 0;
         int usersLife = 10;
 
-        // declaration of array that will store and show chars undecovered by user
-        char [] lettersHit = "_______".toCharArray();
+        File file = new File("hangman first word -test.txt");
+        Scanner fileScanner = new Scanner(file);
 
-        Scanner input = new Scanner(System.in);
+        while (fileScanner.hasNextLine()) {
+            String word = fileScanner.nextLine();
+            System.out.println(word);
+            // declaration of array that will store and show chars undecovered by user
+            char[] lettersHit = "_______".toCharArray();
 
-        //declaration of char that will store the letter entered by user
-        char userChoice;
+            Scanner input = new Scanner(System.in);
 
-        while (usersLife > 0) {
-            /**checks if undercovered letters are equal to the word
-            String.copyValueOf returns a String that represents the character sequence in the array specified.**/
-            if (testWord.equalsIgnoreCase(String.copyValueOf(lettersHit))) {
-                System.out.println("You won the game!");
-                break;
-            }
-            //Scanner reading the user's input which is char
-            userChoice = input.next().charAt(index);
+            //declaration of char that will store the letter entered by user
+            char userChoice;
+
+            while (usersLife > 0) {
+                /**checks if undercovered letters are equal to the word
+                 String.copyValueOf returns a String that represents the character sequence in the array specified.**/
+                if (testWord.equalsIgnoreCase(String.copyValueOf(lettersHit))) {
+                    System.out.println("You won the game!");
+                    break;
+                }
+                //Scanner reading the user's input which is char
+                userChoice = input.next().charAt(index);
 //            contains require the char sequence - that is reason for adding ""
-            if (testWord.contains(userChoice + "")) {
-                System.out.println("You picked the correct word! \n You undercovered a letter.");
-                for (int i = 0; i < testWord.length(); i++) {
-                    // checking if the word contains picked char
-                    if (testWord.charAt(i) == userChoice){
-                        // undercovering picked char/-s
-                        lettersHit[i] = testWord.charAt(i);
-                        System.out.println("The letters are: " + String.copyValueOf(lettersHit));
+                if (testWord.contains(userChoice + "")) {
+                    System.out.println("You picked the correct word! \n You undercovered a letter.");
+                    for (int i = 0; i < testWord.length(); i++) {
+                        // checking if the word contains picked char
+                        if (testWord.charAt(i) == userChoice) {
+                            // undercovering picked char/-s
+                            lettersHit[i] = testWord.charAt(i);
+                            System.out.println("The letters are: " + String.copyValueOf(lettersHit));
+                        }
                     }
-                }
 
-            } else {
-                usersLife--;
+                } else {
+                    usersLife--;
 
-                if (usersLife == 1) {
-                    System.out.println("You are wrong and you loose one life. You have one life left");
+                    if (usersLife == 1) {
+                        System.out.println("You are wrong and you loose one life. You have one life left");
+                    }
+                    System.out.println("You are wrong and you loose one life. You have " + usersLife + " lives left");
                 }
-                System.out.println("You are wrong and you loose one life. You have " + usersLife + " lives left");
             }
-        }
-        if (usersLife == 0) {
-            System.out.println("You have lost!");
-        }
+            if (usersLife == 0) {
+                System.out.println("You have lost!");
+            }
 
+        }
     }
 }
